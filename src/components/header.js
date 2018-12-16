e786a693cd6728da382f551718bf504c7c48bfc3
Jason Smith <jason@engagemore.com>
2018-12-15 23:13:04 -0500

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import styled from 'styled-components';
 
 const HeaderContainer = styled.header`
-  position: fixed;
+  position: absolute;
   top: 1em;
   left: 2em;
   z-index: 100;
 `
 
-const Title = styled.h1`
+const TitleLink = styled(Link)`
+  font-family: 'Anonymous Pro', monospace;
+  font-size: 2em;
+  font-weight: bold;
   margin: 0 0 1em 0;
   color: white;
   text-decoration: none;
-`;
+
+  &:hover {
+    color: #FF0066;
+  }
+`
 
 const Header = ({ siteTitle }) => (
   <HeaderContainer>
-    <Title>
+    <TitleLink to={'/'}>
       {siteTitle}
-    </Title>
+    </TitleLink>
   </HeaderContainer>
 )
 
